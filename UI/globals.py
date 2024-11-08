fb008ae9c7cbc728053623bdf3ea9b6441790a50
Adarshsidnal <01fe21bcs288@kletech.ac.in>
2024-11-08 08:02:01 +0530

    ADDED/UI

---
+++ UI/globals.py
@@ -0,0 +1,5 @@
+board_colour = (181, 136, 99)
+square_size = 75
+
+from_square = None 
+to_square = None
---
+++ UI/sounds.py
@@ -0,0 +1,3 @@
+import pygame.mixer
+pygame.mixer.init()
+move_sound = pygame.mixer.Sound('Assessts/sounds/move.wav')
